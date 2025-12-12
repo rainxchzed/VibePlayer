1d43ea85ec293f34560c2e742c4860e957e3f548
denisdavidek <denis.davidek@gmail.com>
2025-12-12 16:12:53 +0100

    NowPlayingRoot Screen part 3 - progressIndicator, button playback, new resources

--- composeApp/src/commonMain/kotlin/zed/rainxch/vibeplayer/feature/now_playing/presentation/NowPlayingRoot.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/vibeplayer/feature/now_playing/presentation/NowPlayingRoot.kt
@@ -1,24 +1,35 @@
 package zed.rainxch.vibeplayer.feature.now_playing.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
+import vibeplayer.composeapp.generated.resources.Res
+import vibeplayer.composeapp.generated.resources.play
+import vibeplayer.composeapp.generated.resources.skip_next
+import vibeplayer.composeapp.generated.resources.skip_previous
 import zed.rainxch.vibeplayer.core.domain.model.Music
 import zed.rainxch.vibeplayer.feature.main.presentation.MainState
 import zed.rainxch.vibeplayer.feature.main.presentation.MainViewModel
@@ -45,16 +56,66 @@ fun NowPlayingScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.onSecondary)
-            .windowInsetsPadding(WindowInsets.safeDrawing)
+
     ) {
         if (selectedMusic != null)
             MusicContentItem(selectedMusic, modifier = Modifier.align(Alignment.Center))
 
         Column(
-            modifier = Modifier.fillMaxWidth().height(100.dp).background(Color.Blue)
-                .align(Alignment.BottomCenter)
+            modifier = Modifier.fillMaxWidth(0.9f).wrapContentHeight().padding(bottom = 16.dp )
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            LinearProgressIndicator(
+                progress = { 0.57f },
+                modifier = Modifier.fillMaxWidth().wrapContentHeight().padding(bottom = 8.dp),
+                color = MaterialTheme.colorScheme.onSurface,
+                trackColor = MaterialTheme.colorScheme.outline,
+                drawStopIndicator = {})
 
+
+            Row(modifier = Modifier.wrapContentSize().padding(top = 16.dp), verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                IconButton(
+                    onClick = {},
+                    shape = CircleShape,
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primaryFixed,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    modifier = Modifier.size(44.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.skip_previous),
+                        contentDescription = "previous"
+                    )
+                }
+
+                IconButton(onClick = {}, shape = CircleShape, colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.onSurface,
+
+                ), modifier = Modifier.size(60.dp)) {
+                    Icon(
+                        painter = painterResource(Res.drawable.play),
+                        contentDescription = "play/pause"
+                    )
+                }
+
+                IconButton(
+                    onClick = {}, shape = CircleShape,
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primaryFixed,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    modifier = Modifier.size(44.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.skip_next),
+                        contentDescription = "next"
+                    )
+                }
+            }
         }
     }
 }
