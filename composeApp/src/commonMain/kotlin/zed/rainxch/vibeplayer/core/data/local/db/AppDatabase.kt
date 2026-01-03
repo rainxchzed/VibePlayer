4f8359a027612997221c940a19f4731ac1d3f72e
Rainxch Zed <undefineduser087@gmail.com>
2026-01-03 10:54:51 +0500

    feat: Add playlist entities and DAO to database
    - Add `PlaylistEntity` and `PlaylistMusicCrossRef` to the Room database entities.
    - Increment the database version to 3.
    - Expose `playlistDao` in the abstract `AppDatabase` class.

--- composeApp/src/commonMain/kotlin/zed/rainxch/vibeplayer/core/data/local/db/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/vibeplayer/core/data/local/db/AppDatabase.kt
@@ -1,20 +1,27 @@
 package zed.rainxch.vibeplayer.core.data.local.db
 
-import androidx.room.AutoMigration
 import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import zed.rainxch.vibeplayer.core.data.local.db.dao.MusicsDao
+import zed.rainxch.vibeplayer.core.data.local.db.dao.PlaylistDao
 import zed.rainxch.vibeplayer.core.data.local.db.entity.MusicEntity
+import zed.rainxch.vibeplayer.core.data.local.db.entity.PlaylistEntity
+import zed.rainxch.vibeplayer.core.data.local.db.entity.PlaylistMusicCrossRef
 
 @Database(
-    entities = [MusicEntity::class],
+    entities = [
+        MusicEntity::class,
+        PlaylistEntity::class,
+        PlaylistMusicCrossRef::class
+    ],
     version = 3
 )
 @ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract val musicDao: MusicsDao
+    abstract val playlistDao: PlaylistDao
 }
 
-expect object AppDatabaseConstructor: RoomDatabaseConstructor<AppDatabase>
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase>
